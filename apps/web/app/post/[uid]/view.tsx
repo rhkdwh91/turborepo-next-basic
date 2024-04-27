33c2a2519fcdd971e95d5190e2aef497b544c7a1
Kwangyeon.Cho <rhkdwh91@gmail.com>
2024-04-27 15:21:21 +0900

    editor upgrade, viewer skeleton text

--- apps/web/app/post/[uid]/view.tsx
+++ apps/web/app/post/[uid]/view.tsx
@@ -3,7 +3,7 @@
 import { Editor } from "kyz-editor";
 import { useQuery } from "@tanstack/react-query";
 import { queryKeys } from "queryKeys";
-import { Divider, Box, Text, Heading, Stack } from "@chakra-ui/react";
+import { Divider, Box, Text, Heading, Stack, Skeleton } from "@chakra-ui/react";
 import styles from "./page.module.css";
 
 interface ViewProps {
@@ -11,24 +11,26 @@ interface ViewProps {
 }
 
 function View({ uid }: ViewProps) {
-  const { data } = useQuery(queryKeys.posts.detail(uid));
+  const { data, isLoading } = useQuery(queryKeys.posts.detail(uid));
   return (
     <main className={styles.layout}>
-      {data && (
-        <Box position="relative" paddingY="10">
-          <Stack paddingY="2">
-            <Heading fontSize="3xl" padding="2">
-              {data.title}
-            </Heading>
-            <Text paddingX="2">{data.tag}</Text>
-            <Text paddingX="2">{data.userName}</Text>
-            <Text paddingX="2">{data.updateAt}</Text>
-            <Divider padding={2} borderColor={"black"} />
-          </Stack>
+      <Skeleton isLoaded={!isLoading}>
+        {data && (
+          <Box position="relative" paddingY="10">
+            <Stack paddingY="2">
+              <Heading fontSize="3xl" padding="2">
+                {data.title}
+              </Heading>
+              <Text paddingX="2">{data.tag}</Text>
+              <Text paddingX="2">{data.userName}</Text>
+              <Text paddingX="2">{data.updateAt}</Text>
+              <Divider padding={2} borderColor={"black"} />
+            </Stack>
 
-          <Editor editable={false} initialEditorState={data.content} />
-        </Box>
-      )}
+            <Editor editable={false} initialEditorState={data.content} />
+          </Box>
+        )}
+      </Skeleton>
     </main>
   );
 }
