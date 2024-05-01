1522732b577c566e1a0d471813304bfd6a9c9e94
Kwangyeon.Cho <rhkdwh91@gmail.com>
2024-05-01 19:31:25 +0900

    added logo image

--- apps/web/components/ui/organism/Header/index.tsx
+++ apps/web/components/ui/organism/Header/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import Image from "next/image";
 import Link from "next/link";
 import styles from "./styles.module.css";
 import LogoutButton from "components/ui/atom/LogoutButton";
@@ -9,12 +10,12 @@ import useUserInfo from "hooks/useUserInfo";
 export default function Header() {
   const { isLogin, userInfo } = useUserInfo();
   return (
-    <Box bg="tomato">
+    <Box bg="black">
       <Box
         color="white"
         display={"flex"}
         maxWidth={{ md: "100%", xl: 1160 }}
-        justifyContent="flex-end"
+        justifyContent="space-between"
         alignItems="center"
         flexWrap={"wrap"}
         margin={"0 auto 30px auto"}
@@ -22,22 +23,25 @@ export default function Header() {
         boxSize={"border-box"}
         padding={4}
       >
-        {isLogin ? (
-          <>
-            <Link href={"/post/write"} className={styles.link}>
-              Write
-            </Link>
-            <Text>Hello, {userInfo.username}!</Text>
-            <LogoutButton />
-          </>
-        ) : (
-          <>
-            <Link href={"/sign-in"} className={styles.link}>
-              SignIn
-            </Link>
-            <Text>Hello, guest!</Text>
-          </>
-        )}
+        <Image width={120} height={30} src={"/logo.png"} alt={"logo"} />
+        <Flex alignItems={"center"} gap={"4"}>
+          {isLogin ? (
+            <>
+              <Link href={"/post/write"} className={styles.link}>
+                Write
+              </Link>
+              <Text>Hello, {userInfo.username}!</Text>
+              <LogoutButton />
+            </>
+          ) : (
+            <>
+              <Link href={"/sign-in"} className={styles.link}>
+                SignIn
+              </Link>
+              <Text>Hello, guest!</Text>
+            </>
+          )}
+        </Flex>
       </Box>
     </Box>
   );
