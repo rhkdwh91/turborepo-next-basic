5d3f2493415187ed1fb257e53eec1239727b2c68
Kwangyeon.Cho <rhkdwh91@gmail.com>
2024-05-04 09:07:34 +0900

    Added tags

---
+++ apps/web/app/api/tag/route.ts
@@ -0,0 +1,47 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import prisma from "prisma/client";
+import { cloneDeep } from "lodash";
+import authCheck from "utils/authCheck";
+
+export async function GET() {
+  try {
+    const tags = await prisma.tag.findMany({
+      take: 10,
+      orderBy: {
+        createAt: "desc",
+      },
+    });
+    return NextResponse.json(tags, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
+}
+
+export async function POST(req: NextRequest) {
+  try {
+    const user = await authCheck(req);
+    if (!user) {
+      return NextResponse.json(
+        { message: "No user found." },
+        {
+          status: 401,
+          headers: {
+            "Set-Cookie": `accessToken=;Path=/;HttpOnly;Max-Age=0;`,
+          },
+        },
+      );
+    }
+    const requestData = await req.json();
+    const form = cloneDeep(requestData);
+    await prisma.tag.create({ data: form });
+    return NextResponse.json(
+      { message: "tag updated successful!" },
+      { status: 201 },
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
+}
