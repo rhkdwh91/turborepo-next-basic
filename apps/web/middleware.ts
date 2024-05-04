8bec43c28240f6f39f4534b9c9a39168b66dd4a2
Kwangyeon.Cho <rhkdwh91@gmail.com>
2024-05-04 23:08:28 +0900

    Added tag

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,8 +1,6 @@
 import { withAuth } from "next-auth/middleware";
 
-export default withAuth(function (req) {
-  console.log(req);
-}, {});
+export default withAuth(function () {}, {});
 
 export const config = {
   matcher: ["/post/write", "/post/modify/:path*", "/my"],
