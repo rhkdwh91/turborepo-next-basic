5c23257ed6fd1ee26d699ed340d80815321157ba
Kwangyeon.Cho <rhkdwh91@gmail.com>
2024-06-02 10:27:10 +0900

    Add GET method in users route
    Added a GET method in the users route that checks for user authentication, retrieves users from the database and handles any emerging errors. Pagination and ordering parameters have been included to better handle large amounts of data.

---
+++ apps/web/app/api/users/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import prisma from "prisma/client";
+import authCheck from "@/utils/authCheck";
+import { errorHandler } from "@/utils/apiErrorHandler";
+
+export async function GET(req: NextRequest) {
+  try {
+    await authCheck(req);
+    const searchParams = req.nextUrl.searchParams;
+    const take = searchParams.get("take");
+    const skip = searchParams.get("skip");
+    const posts = await prisma.user.findMany({
+      take: take && !Number.isNaN(Number(take)) ? Number(take) : 100,
+      skip: skip && !Number.isNaN(Number(skip)) ? Number(skip) : 0,
+      orderBy: {
+        uid: "desc",
+      },
+    });
+    return NextResponse.json(posts, { status: 200 });
+  } catch (error) {
+    return errorHandler(error);
+  }
+}
