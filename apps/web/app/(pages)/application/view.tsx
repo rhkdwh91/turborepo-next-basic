0bc0f5b11f03a06791670bfd59806fabdef23d2c
Kwangyeon.Cho <rhkdwh91@gmail.com>
2024-06-25 21:19:41 +0900

    Update application form UI in web app
    The form UI in the application view of the web app has been updated for improved usability and visual appeal. This includes fixing a typo in the field name 'reeson' to 'reason', adding styling classes to various elements, and replacing the standard HTML button with a Chakra UI Button. We also made changes to ensure the display of character count is consistent.

--- apps/web/app/(pages)/application/view.tsx
+++ apps/web/app/(pages)/application/view.tsx
@@ -1,27 +1,36 @@
 "use client";
 
 import { FormProvider, useForm } from "react-hook-form";
+import { Button } from "@chakra-ui/react";
 
 export default function View() {
   const methods = useForm();
 
-  const reasonWatch = methods.watch("reeson");
+  const reasonWatch = methods.watch("reason");
 
   return (
-    <main>
-      <h1>작가신청</h1>
-      <em>
+    <main className="max-w-[1160px] mx-auto p-5">
+      <h1 className="font-bold text-2xl mb-3">작가신청</h1>
+      <em className="block mb-4 text-gray-800 not-italic">
         KLOG 에서 어떤 이야기를 써내려가고 싶으세요? (1500자 이내로
         작성해주세요.)
       </em>
       <FormProvider {...methods}>
-        <form>
+        <form className="w-[500px] block">
           <textarea
+            className="my-2 p-5 w-[500px] h-[500px] border border-gray-200 rounded-md text-sm leading-tight text-black"
             {...methods.register("reason", { required: true, maxLength: 1500 })}
             required
           />
-          <div>{(reasonWatch && reasonWatch.length) ?? 0} 자</div>
-          <button>신청</button>
+          <div>{reasonWatch ? reasonWatch.length : 0} 자</div>
+          <Button
+            type="submit"
+            color="white"
+            bgColor="cornflowerblue"
+            className="float-right"
+          >
+            신청
+          </Button>
         </form>
       </FormProvider>
     </main>
