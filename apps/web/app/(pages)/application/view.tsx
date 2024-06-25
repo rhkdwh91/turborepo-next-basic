71854083a74c2436c8175c7074f7980badd03929
Kwangyeon.Cho <rhkdwh91@gmail.com>
2024-06-25 19:11:33 +0900

    Add form to application view
    The application view now has a form powered by react-hook-form, which includes a textarea for user input. The user now needs to provide a reason, with a maximum length of 1500 characters, when submitting the application.

--- apps/web/app/(pages)/application/view.tsx
+++ apps/web/app/(pages)/application/view.tsx
@@ -1,8 +1,29 @@
+"use client";
+
+import { FormProvider, useForm } from "react-hook-form";
+
 export default function View() {
+  const methods = useForm();
+
+  const reasonWatch = methods.watch("reeson");
+
   return (
     <main>
       <h1>작가신청</h1>
-      <button>신청</button>
+      <em>
+        KLOG 에서 어떤 이야기를 써내려가고 싶으세요? (1500자 이내로
+        작성해주세요.)
+      </em>
+      <FormProvider {...methods}>
+        <form>
+          <textarea
+            {...methods.register("reason", { required: true, maxLength: 1500 })}
+            required
+          />
+          <div>{(reasonWatch && reasonWatch.length) ?? 0} 자</div>
+          <button>신청</button>
+        </form>
+      </FormProvider>
     </main>
   );
 }
