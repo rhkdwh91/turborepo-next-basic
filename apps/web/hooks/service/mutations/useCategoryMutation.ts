c048acec791736cb9f8a9e3843aaaba700593da8
Kwangyeon.Cho <rhkdwh91@gmail.com>
2024-06-28 22:02:25 +0900

    Add category create and delete mutations
    A new file, useCategoryMutation.ts, has been added in the hooks/service/mutations directory. This file includes two mutations - useCategoryCreateMutation and useCategoryDeleteMutation - used for creating and deleting categories respectively via API calls. A success toast message has been added to signify successful deletion.

---
+++ apps/web/hooks/service/mutations/useCategoryMutation.ts
@@ -0,0 +1,20 @@
+import axiosInstance from "axiosInstance";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "kyz-toast";
+
+export const useCategoryCreateMutation = () => {
+  return useMutation({
+    mutationFn: (payload: { value: string; name: string }) =>
+      axiosInstance.post("/api/categories", payload),
+  });
+};
+
+export const useCategoryDeleteMutation = () => {
+  return useMutation({
+    mutationFn: (name: string) =>
+      axiosInstance.delete(`/api/categories/${name}`),
+    onSuccess: () => {
+      toast.success("Successfully delete post!");
+    },
+  });
+};
