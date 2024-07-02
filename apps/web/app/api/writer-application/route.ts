a95a32a79283f498f23d5cbe9882143aff3af766
Kwangyeon.Cho <rhkdwh91@gmail.com>
2024-07-02 19:04:30 +0900

    Add POST route for writer application
    A POST route for the writer application has been introduced in the route.ts file. This route includes authentication checks, data cloning and handling, as well as error management. It uses Next.js server-side handlers along with Prisma for database interaction.

---
+++ apps/web/app/api/writer-application/route.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from "next/server";
+import authCheck from "@/utils/authCheck";
+import { cloneDeep } from "lodash";
+import prisma from "@/prisma/client";
+import { errorHandler } from "@/utils/apiErrorHandler";
+
+export async function POST(req: NextRequest) {
+  try {
+    const user = await authCheck(req);
+    const requestData = await req.json();
+    const form = cloneDeep(requestData);
+    form.userUid = user.uid;
+    await prisma.writerApplication.create({ data: form });
+    return NextResponse.json({ message: "ok" }, { status: 201 });
+  } catch (error) {
+    return errorHandler(error);
+  }
+}
