344e2db5ef2f6f0f3c857eed4131431e0471d963
Kwangyeon.Cho <rhkdwh91@gmail.com>
2024-07-11 21:32:43 +0900

    Add list query to writerApplicationKeys
    The writerApplicationKeys object in the queryKeys directory now includes a list query. This query asynchronously retrieves data from the "/api/writer-application" endpoint, specifically returning an array of WriterApplication objects.

--- apps/web/queryKeys/writerApplicationKeys.ts
+++ apps/web/queryKeys/writerApplicationKeys.ts
@@ -3,6 +3,13 @@ import axiosInstance from "axiosInstance";
 import { WriterApplication } from "types/writer-application";
 
 const writerApplicationKeys = createQueryKeys("writerApplication", {
+  list: () => ({
+    queryKey: ["list"],
+    queryFn: async (): Promise<WriterApplication[]> => {
+      const { data } = await axiosInstance.get(`/api/writer-application`);
+      return data;
+    },
+  }),
   detail: () => ({
     queryKey: ["detail"],
     queryFn: async (): Promise<WriterApplication> => {
