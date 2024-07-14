d26a75f774a9c11af3d56e27456abd046150d600
Kwangyeon.Cho <rhkdwh91@gmail.com>
2024-07-14 21:10:33 +0900

    Remove migration lock and simplify writer application logic
    Removed the migration_lock.toml file which was redundant. Also, the complex logic to create a new writer application was simplified by directly creating a new writer application from the form data, removing the unnecessary check for a pre-existing application. Also, added error console log for better error tracking.

--- apps/web/app/api/writer-application/route.ts
+++ apps/web/app/api/writer-application/route.ts
@@ -39,20 +39,13 @@ export async function POST(req: NextRequest) {
     const requestData = await req.json();
     const form = cloneDeep(requestData);
     form.userUid = user.uid;
-    const writerApplication = await prisma.writerApplication.findUnique({
-      where: {
-        userUid: user.uid,
-      },
+    await prisma.writerApplication.create({
+      data: form,
     });
-    if (writerApplication) {
-      return NextResponse.json(
-        { message: "이미 있는 application form" },
-        { status: 409 },
-      );
-    }
-    await prisma.writerApplication.create({ data: form });
+
     return NextResponse.json({ message: "ok" }, { status: 201 });
   } catch (error) {
+    console.log(error);
     return errorHandler(error);
   }
 }
