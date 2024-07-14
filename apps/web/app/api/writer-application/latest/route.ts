d8823b16b2edae3e6e08070f4818f2cfd1c5ae48
Kwangyeon.Cho <rhkdwh91@gmail.com>
2024-07-14 19:57:21 +0900

    Replace findUnique with findFirst in writerApplication query
    In the writerApplication route.ts for the web app API, the Prisma query method findUnique has been replaced with findFirst. This change ensures that the query will fetch the first record matching criteria, which is more suitable for our use-case compared to the earlier used findUnique method.

--- apps/web/app/api/writer-application/latest/route.ts
+++ apps/web/app/api/writer-application/latest/route.ts
@@ -6,7 +6,7 @@ import { errorHandler } from "@/utils/apiErrorHandler";
 export async function GET(req: NextRequest) {
   try {
     const user = await authCheck(req);
-    const writerApplication = await prisma.writerApplication.findUnique({
+    const writerApplication = await prisma.writerApplication.findFirst({
       where: {
         userUid: user.uid,
       },
