94e0dfea3e17989a206f9e05ad17a69ad1263e04
Kwangyeon.Cho <rhkdwh91@gmail.com>
2024-07-16 17:54:47 +0900

    Add mutation hooks for writer application status
    The hooks useMutation for 'useAcceptingWriterApplication', 'useAcceptWriterApplication', and 'useRejectWriterApplication' were added. Each hook resorts to an axios request to post the writer application status to the "/api/writer-application" endpoint. They display a success toast message upon successful request execution.

--- apps/web/hooks/service/mutations/useWriterApplication.ts
+++ apps/web/hooks/service/mutations/useWriterApplication.ts
@@ -17,3 +17,51 @@ export const useReceiptWriterApplication = () => {
     },
   });
 };
+
+export const useAcceptingWriterApplication = () => {
+  return useMutation({
+    mutationFn: async (payload: { content: string }) => {
+      const { data } = await axiosInstance.post("/api/writer-application", {
+        ...payload,
+        status: "ACCEPTING",
+      });
+
+      return data;
+    },
+    onSuccess: () => {
+      toast.success("Successfully registered user!");
+    },
+  });
+};
+
+export const useAcceptWriterApplication = () => {
+  return useMutation({
+    mutationFn: async (payload: { content: string }) => {
+      const { data } = await axiosInstance.post("/api/writer-application", {
+        ...payload,
+        status: "ACCEPT",
+      });
+
+      return data;
+    },
+    onSuccess: () => {
+      toast.success("Successfully registered user!");
+    },
+  });
+};
+
+export const useRejectWriterApplication = () => {
+  return useMutation({
+    mutationFn: async (payload: { content: string }) => {
+      const { data } = await axiosInstance.post("/api/writer-application", {
+        ...payload,
+        status: "REJECT",
+      });
+
+      return data;
+    },
+    onSuccess: () => {
+      toast.success("Successfully registered user!");
+    },
+  });
+};
