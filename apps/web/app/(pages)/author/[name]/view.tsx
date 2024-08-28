03cc398cf422bb6f2a32128ce7aa92b78bd34d8f
Kwangyeon.Cho <rhkdwh91@gmail.com>
2024-08-28 21:16:44 +0900

    Enhance author profile page layout and styling
    Added ProfileImage component to display author profile images. Improved layout and styling of author posts and profile to enhance readability and user experience.

--- apps/web/app/(pages)/author/[name]/view.tsx
+++ apps/web/app/(pages)/author/[name]/view.tsx
@@ -2,6 +2,7 @@
 
 import { useAuthorQuery } from "hooks/service/queries/useAuthorQuery";
 import { usePostListQuery } from "hooks/service/queries/usePostQuery";
+import ProfileImage from "@repo/ui/components/atom/ProfileImage";
 
 interface PostListProps {
   username: string;
@@ -9,7 +10,20 @@ interface PostListProps {
 
 function PostList({ username }: PostListProps) {
   const { data: postList } = usePostListQuery({ skip: 0, take: 100, username });
-  return <section>{postList?.map((post) => <div>{post.title}</div>)}</section>;
+  return (
+    <section className="my-20">
+      <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+        작가 포스트
+      </h3>
+      <div className="flex flex-wrap gap-10 mt-10">
+        {postList?.map((post) => (
+          <span className="scroll-m-20 text-xl font-semibold tracking-tight">
+            {post.title}
+          </span>
+        ))}
+      </div>
+    </section>
+  );
 }
 
 interface ViewProps {
@@ -19,12 +33,18 @@ interface ViewProps {
 export default function View({ name }: ViewProps) {
   const { data } = useAuthorQuery(name);
   return (
-    <main>
+    <main className="min-h-screen max-w-6xl mx-auto">
       {data && (
         <>
-          <article>
-            <h1>{data.username} 작가 프로필</h1>
-            <img src={data.profileImage ?? ""} alt="profile image" />
+          <article className="flex gap-10 items-center flex-nowrap mt-20">
+            <ProfileImage
+              height={70}
+              width={70}
+              src={data.profileImage ?? ""}
+            />
+            <h1 className="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-4xl">
+              {data.username} 작가 프로필
+            </h1>
           </article>
           <PostList username={data.username} />
         </>
