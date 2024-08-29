0d17f3148111e1b2c426e6b1ff1b5c53c3437dd1
Kwangyeon.Cho <rhkdwh91@gmail.com>
2024-08-29 22:22:50 +0900

    Increase skeleton component height for consistency
    Updated the height of all skeleton components from 230px to 271px. This ensures a uniform appearance across the entire UI and enhances visual consistency for users.

--- apps/web/components/ui/skeleton/PostsSkeleton/index.tsx
+++ apps/web/components/ui/skeleton/PostsSkeleton/index.tsx
@@ -3,12 +3,12 @@ import { Skeleton } from "@ui/src/components/atom/Skeleton";
 export default function PostsSkeleton() {
   return (
     <>
-      <Skeleton className="h-[230px] w-[250px] rounded-xl" />
-      <Skeleton className="h-[230px] w-[400px] rounded-xl" />
-      <Skeleton className="h-[230px] w-[300px] rounded-xl" />
-      <Skeleton className="h-[230px] w-[250px] rounded-xl" />
-      <Skeleton className="h-[230px] w-[320px] rounded-xl" />
-      <Skeleton className="h-[230px] w-[200px] rounded-xl" />
+      <Skeleton className="h-[271px] w-[250px] rounded-xl" />
+      <Skeleton className="h-[271px] w-[400px] rounded-xl" />
+      <Skeleton className="h-[271px] w-[300px] rounded-xl" />
+      <Skeleton className="h-[271px] w-[250px] rounded-xl" />
+      <Skeleton className="h-[271px] w-[320px] rounded-xl" />
+      <Skeleton className="h-[271px] w-[200px] rounded-xl" />
     </>
   );
 }
