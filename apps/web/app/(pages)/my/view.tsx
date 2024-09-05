fa928636b705b35070b01d0e5d5a80b016545866
Kwangyeon.Cho <rhkdwh91@gmail.com>
2024-09-06 00:08:20 +0900

    Update profile page to use session and display posts
    Switched from react-query to next-auth session for user data retrieval and incorporated a new hook to fetch and display user's posts. Enhanced UI elements and adjusted styling for better visual hierarchy.

--- apps/web/app/(pages)/my/view.tsx
+++ apps/web/app/(pages)/my/view.tsx
@@ -1,32 +1,53 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
-import { queryKeys } from "queryKeys";
+import { useSession } from "next-auth/react";
 import ProfileImage from "@ui/src/components/atom/ProfileImage";
+import { usePostListQuery } from "@/hooks/service/queries/usePostQuery";
 
 export default function View() {
-  const { data } = useQuery(queryKeys.profile.detail());
+  const { data } = useSession();
+  const { data: postList } = usePostListQuery({
+    skip: 0,
+    take: 100,
+    username: data?.user?.username,
+  });
 
   return (
     <main className="max-w-6xl mx-auto min-h-screen">
-      <div className="pt-10">
-        <h1 className="text-3xl font-semibold pb-4">내 정보</h1>
-        {data && (
-          <div className="flex gap-2 pb-4 mb-4 border-b border-gray-700">
-            {data.profileImage && (
-              <ProfileImage src={data.profileImage} width={80} height={80} />
+      <div className="pt-20">
+        <h1 className="text-3xl font-semibold pb-10">내 정보</h1>
+        {data?.user && (
+          <div className="flex gap-2 pb-10 mb-4 border-b border-gray-700">
+            {data.user.profileImage && (
+              <ProfileImage
+                src={data.user.profileImage}
+                width={80}
+                height={80}
+              />
             )}
             <div>
               <p>
-                level: {data.level === 1 && "최고 관리자"}{" "}
-                {data.level === 2 && "작가"} {data.level === 3 && "회원"}
+                회원 등급: {data.user.level === 1 && "최고 관리자"}{" "}
+                {data.user.level === 2 && "작가"}{" "}
+                {data.user.level === 3 && "회원"}
               </p>
-              <p>email: {data.email}</p>
-              <p>username: {data.username}</p>
+              <p>email: {data.user.email}</p>
+              <p>username: {data.user.username}</p>
             </div>
           </div>
         )}
-        <h2 className="text-2xl font-semibold pb-4">내 포스트</h2>
+        <section className="mt-10">
+          <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+            내 포스트
+          </h3>
+          <div className="flex flex-wrap gap-10 mt-10">
+            {postList?.map((post) => (
+              <span className="scroll-m-20 text-xl font-semibold tracking-tight">
+                {post.title}
+              </span>
+            ))}
+          </div>
+        </section>
       </div>
     </main>
   );
