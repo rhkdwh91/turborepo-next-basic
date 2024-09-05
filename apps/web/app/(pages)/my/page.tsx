1a70f7fec04ccefceddfaa8c236237654df1d8e2
Kwangyeon.Cho <rhkdwh91@gmail.com>
2024-09-06 00:12:11 +0900

    Prefetch posts data in my/page component
    Added a prefetch query for a list of posts in addition to the profile details. This ensures the posts data is available on initial render, improving page load performance. The session user's username is used as a parameter for fetching the posts.

--- apps/web/app/(pages)/my/page.tsx
+++ apps/web/app/(pages)/my/page.tsx
@@ -2,10 +2,22 @@ import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { getQueryClient, queryKeys } from "queryKeys";
 
 import View from "./view";
+import { getServerSession } from "next-auth";
+import authOptions from "@/auth.config";
 
 export default async function Page() {
+  const session = await getServerSession(authOptions);
   const queryClient = getQueryClient();
-  await queryClient.prefetchQuery(queryKeys.profile.detail());
+  await Promise.all([
+    queryClient.prefetchQuery(queryKeys.profile.detail()),
+    queryClient.prefetchQuery({
+      ...queryKeys.posts.list({
+        skip: 0,
+        take: 100,
+        username: session?.user?.username,
+      }),
+    }),
+  ]);
 
   const dehydratedState = dehydrate(queryClient);
   return (
